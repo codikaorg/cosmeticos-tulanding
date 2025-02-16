5a106a6f07fb1ad6cf981dc19a0fb24e722239cd
codikalabs <codikalabsve@gmail.com>
2025-02-16 15:17:38 -0400

    gris en el fondo de imagen

--- src/components/DeliCombos.tsx
+++ src/components/DeliCombos.tsx
@@ -46,7 +46,7 @@ const DeliCombos = () => {
                 <img
                   src={product.image}
                   alt={product.name}
-                  className="w-full h-54 object-cover rounded-t-lg"
+                  className="w-full h-54 object-cover rounded-t-lgb bg-gray-200"
                 />
               </CardHeader>
               <CardContent className="p-6">
